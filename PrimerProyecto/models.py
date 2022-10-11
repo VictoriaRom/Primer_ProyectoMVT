d3da12093d32f772a06e23ef58218dbf857268a3
Victoria Romero <97700058+VictoriaRom@users.noreply.github.com>
2022-10-11 00:45:35 -0300

    Update models.py

--- PrimerProyecto/models.py
+++ PrimerProyecto/models.py
@@ -5,5 +5,5 @@ class Persona(models.Model):
     nombre= models.CharField(max_length=30)  #Nombre del familiar
     apellido=models.CharField(max_length=30) #Apellido del familiar
     edad=models.IntegerField()                #Edad del familiar
-    fecha_nacimiento=models.DateField(null=True) #fecha de nacimiento del familiar
+    fecha_registro=models.DateField()        #fecha 
     email=models.CharField(max_length=30)         #correo electronico del familiar
